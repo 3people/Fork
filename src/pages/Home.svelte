c0bd6f0609e13db85565c0fc53960d4f6196466f
3people <smsn7031@naver.com>
2024-07-24 01:56:30 +0900

    feat: add home page

---
+++ src/pages/Home.svelte
@@ -0,0 +1,32 @@
+<div class="w-[22.5rem] mt-8 px-4">
+    <div class="flex">
+        <div class="pr-4">logo</div>
+        <div class="flex gap-4">
+            <div>인파예보</div>
+            <div>추천장소</div>
+        </div>
+    </div>
+    <div class="mt-4">
+        현재위치 인디케이터
+    </div>
+    <div class="mt-8">
+        지금 삼평동은 사람 많아요~
+    </div>
+    <div class="mt-0.5 flex flex-col">
+        <span>현재시간 업데이트</span>
+        <span class="mt-0.5">실시간 혼잡도 분석</span>
+        <div class="flex flex-col mt-4">
+            <span>혼잡도</span>
+            <span>4.2/5.0</span>
+        </div>
+    </div>
+    <div class="flex flex-col mt-12">
+        <span>시간 별 혼잡도 예보</span>
+        <span class="mt-0.5">지난 데이터와 날씨, 공휴일 및 특이사항을 종합하여 AI가 예측한 결과입니다.</span>
+        <div class="mt-1">차트</div>
+    </div>
+    <div class="mt-8">
+        <span>이번 주 삼평동 인파예보</span>
+        <div class="mt-4">예보 위치</div>
+    </div>
+</div>
