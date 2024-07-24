f109cd5796e82d299d67d431e33d82863c660fd2
3people <smsn7031@naver.com>
2024-07-24 17:03:10 +0900

    fix: home page

--- src/pages/Home.svelte
+++ src/pages/Home.svelte
@@ -1,16 +1,26 @@
+<script lang="ts">
+    import Poi from "../assets/icons/Poi.svelte";
+    import ArrowDown from "../assets/icons/ArrowDown.svelte";
+
+    let currentLocation = '신대방동'
+</script>
 <div class="w-[22.5rem] mt-8 px-4">
     <div class="flex">
         <div class="pr-4">logo</div>
-        <div class="flex gap-4">
-            <div>인파예보</div>
+        <div class="flex gap-4 font-bold text-lg [&>div]:pb-[0.375rem]">
+            <div class="border-solid border-b-2 border-b-black">인파예보</div>
             <div>추천장소</div>
         </div>
     </div>
-    <div class="mt-4">
-        현재위치 인디케이터
+    <div class="mt-4 bg-[#f9f9fb] rounded-xl px-2.5 py-3">
+        <div class="flex items-center">
+            <Poi />
+            <span class="ml-1">{currentLocation}</span>
+            <ArrowDown />
+        </div>
     </div>
     <div class="mt-8">
-        지금 삼평동은 사람 많아요~
+        지금 {currentLocation}은 사람 많아요~
     </div>
     <div class="mt-0.5 flex flex-col">
         <span>현재시간 업데이트</span>
@@ -26,7 +36,7 @@
         <div class="mt-1">차트</div>
     </div>
     <div class="mt-8">
-        <span>이번 주 삼평동 인파예보</span>
+        <span>이번 주 {currentLocation} 인파예보</span>
         <div class="mt-4">예보 위치</div>
     </div>
 </div>
