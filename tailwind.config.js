24f73073aa32520096897f0c452dc2434c9310eb
3people <smsn7031@naver.com>
2024-08-19 02:17:05 +0900

    feat: new fonts, icons, images

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,22 @@ export default {
     extend: {
       fontFamily: {
         pretendard: ["pretendard"],
+        line: ['line']
       },
+      colors: {
+        'black': {
+          'primary': '#0a0a0a',
+          'secondary': 'rgba(10,10,10,0.7)',
+          'tertiary': 'rgba(10,10,10,0.5)',
+        },
+        'gray': {
+          '100': '#fbfbfb',
+          '200': '#f5f5f5'
+        },
+        'brand': {
+          'point': '#FF4A22'
+        }
+      }
     },
   },
   plugins: [],
