5fba58c6b9463560dd8bf698d4225818b8d9e378
3people <smsn7031@naver.com>
2024-08-25 18:58:20 +0900

    fix: eslint

--- src/requests/fetch/translate.ts
+++ src/requests/fetch/translate.ts
@@ -1,51 +1,26 @@
-import OpenAI from 'openai'
-import { config } from '../../config/api-key'
-
-const openai = new OpenAI({
-  apiKey: config.OPENAI_API_KEY,
-})
-
-// export const translate = async () => {
-//     const completion = await openai.chat.completions.create({
-//         model: "gpt-3.5-turbo",
-//         messages: [
-//             { role: "system", content: "You are a helpful assistant." },
-//             {
-//                 role: "user",
-//                 content: "Write a haiku about recursion in programming.",
-//             },
-//         ],
-//     })
-//
-//     console.log(completion.choices[0].message);
-// }
+import {config} from '../../config/api-key'
 
 export const translate = async () => {
-  const response = await fetch(
-    'https://api.openai.com/v1/chat/completions',
-    {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        Authorization: `Bearer ${config.OPENAI_API_KEY}`,
-      },
-      body: JSON.stringify({
-        model: 'gpt-3.5-turbo',
-        messages: [
-          {
-            role: 'system',
-            content:
-              'You are a helpful assistant.',
-          },
-          {
-            role: 'user',
-            content:
-              'Write a haiku about recursion in programming.',
-          },
-        ],
-      }),
+  const response = await fetch('https://api.openai.com/v1/chat/completions', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${config.OPENAI_API_KEY}`,
     },
-  )
+    body: JSON.stringify({
+      model: 'gpt-3.5-turbo',
+      messages: [
+        {
+          role: 'system',
+          content: 'You are a helpful assistant.',
+        },
+        {
+          role: 'user',
+          content: 'Write a haiku about recursion in programming.',
+        },
+      ],
+    }),
+  })
 
   return response.json()
 }
