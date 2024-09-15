0fd7775f967fbe1e32136da2a2c6520947457b0b
3people <smsn7031@naver.com>
2024-09-15 23:57:56 +0900

    feat: search page(WIP)

---
+++ src/pages/SearchPage.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+  import {querystring} from 'svelte-spa-router'
+  import {parseQueryString} from '../utils/url'
+
+  $: keyword = parseQueryString($querystring ?? '')?.keyword
+</script>
+
+<div>keyword: {keyword}</div>
