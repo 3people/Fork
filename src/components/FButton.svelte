b4a78da58f87b944663f8887c06ed54efa407cf3
3people <smsn7031@naver.com>
2024-09-15 23:57:47 +0900

    fix: use clsx

--- src/components/FButton.svelte
+++ src/components/FButton.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
   import {createEventDispatcher} from 'svelte'
+  import {clsx} from 'clsx'
 
   const dispatch = createEventDispatcher()
   const onClick = (event: any) => {
@@ -9,7 +10,7 @@
 
 <button
   {...$$props}
-  class={`w-full rounded-lg bg-brand-point text-white font-bold px-20 py-4 ${$$props.class ? ` ${$$props.class}` : ''}`}
+  class={clsx($$props.class, 'w-full rounded-lg bg-brand-point text-white font-bold px-20 py-4')}
   on:click={(event) => onClick(event)}
 >
   <slot />
