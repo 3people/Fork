faab021a41e909e989394f0b7cafd6ff0b90e363
3people <smsn7031@naver.com>
2024-09-22 02:37:31 +0900

    fix: landing page

--- src/pages/landing/_components/Splash.svelte
+++ src/pages/landing/_components/Splash.svelte
@@ -1,7 +1,8 @@
-<script lang=ts>
-    import SplashLogo from "../../../assets/images/SplashLogo.svelte"
+<script lang="ts">
+  import SplashLogo from '../../../assets/images/SplashLogo.svelte'
 </script>
-<div class="absolute inset-0 flex flex-col justify-center items-center bg-brand-point">
-    <SplashLogo/>
-    <span class="text-white mt-[0.75rem]">콕 찍어먹는 한식 여행 번역기</span>
+
+<div class="absolute inset-0 flex flex-col justify-center items-center bg-brand-point z-20">
+  <SplashLogo />
+  <span class="text-white mt-[0.75rem]">콕 찍어먹는 한식 여행 번역기</span>
 </div>
