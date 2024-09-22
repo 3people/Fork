f705d0456c9255d6255ffa0793a48db66572432e
3people <smsn7031@naver.com>
2024-09-23 02:12:08 +0900

    fix: hide dropdown on landing page

--- src/components/FHeader.svelte
+++ src/components/FHeader.svelte
@@ -1,6 +1,7 @@
 <script>
   import ForkLogo from '../assets/icons/ForkLogo.svelte'
   import FDropdown from './FDropdown.svelte'
+  import {location} from 'svelte-spa-router'
 </script>
 
 <header class="px-4 py-2 flex justify-between w-[22.5rem] h-12 fixed top-0 z-10 bg-white">
@@ -8,5 +9,7 @@
     <ForkLogo />
     <span class="font-santokki text-brand-point leading-6">포크</span>
   </div>
-  <FDropdown />
+  {#if $location !== '/'}
+    <FDropdown />
+  {/if}
 </header>
