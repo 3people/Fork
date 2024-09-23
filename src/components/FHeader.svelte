0819273f6610c151f945d34b20ca57081fc177d5
3people <smsn7031@naver.com>
2024-09-23 23:15:08 +0900

    fix: i18n

--- src/components/FHeader.svelte
+++ src/components/FHeader.svelte
@@ -2,14 +2,17 @@
   import ForkLogo from '../assets/icons/ForkLogo.svelte'
   import FDropdown from './FDropdown.svelte'
   import {location} from 'svelte-spa-router'
+  import {_} from 'svelte-i18n'
+
+  $: displayDropdown = !['/', '/restaurant'].includes($location)
 </script>
 
 <header class="px-4 py-2 flex justify-between w-[22.5rem] h-12 fixed top-0 z-10 bg-white">
   <div class="flex gap-0.5 items-center">
     <ForkLogo />
-    <span class="font-santokki text-brand-point leading-6">포크</span>
+    <span class="font-santokki text-brand-point leading-6">{$_('header.title')}</span>
   </div>
-  {#if $location !== '/'}
+  {#if displayDropdown}
     <FDropdown />
   {/if}
 </header>
--- src/components/FInfo.svelte
+++ src/components/FInfo.svelte
@@ -1,18 +1,18 @@
 <script lang="ts">
-  import type {
-    RestaurantCommonInfo,
-    RestaurantDetailInfo,
-  } from '../requests/normalize/restaurant-info'
   import {createEventDispatcher} from 'svelte'
+  import {_, locale} from 'svelte-i18n'
+  import {location} from 'svelte-spa-router'
 
   export let type: string = '식당'
-  export let item: RestaurantDetailInfo & RestaurantCommonInfo
+  export let item: any
   export let flow: 'vertical' | 'horizontal' = 'horizontal'
 
   const dispatch = createEventDispatcher()
 
   const onClick = () => {
-    dispatch('click', {contentId: item.contentId})
+    dispatch('click', {
+      contentId: $location !== '/search' ? item.relation[$locale ?? 'ko'] : item.contentId,
+    })
   }
 </script>
 
@@ -29,8 +29,17 @@
   />
   <div class="flex flex-col gap-1 text-left">
     <span class="text-brand-point font-bold text-[0.625rem]">{type}</span>
-    <span class="font-bold text-base">{item.title}</span>
-    <span class="text-xs text-black-tertiary">{@html item.openTime}</span>
-    <span class="text-xs text-black-secondary">{item.firstMenu}</span>
+    {#if $location === '/search'}
+      <span class="font-bold text-base">{item.title}</span>
+      <span class="text-xs text-black-tertiary">{@html item.openTime}</span>
+      <span class="text-xs text-black-secondary">{item.firstMenu}</span>
+    {:else}
+      <span class="font-bold text-base">{$_(`home.info.${item.contentId}.title`)}</span>
+      <span class="text-xs text-black-tertiary">
+        {@html $_(`home.info.${item.contentId}.openTime`)}
+      </span>
+      <span class="text-xs text-black-secondary">{$_(`home.info.${item.contentId}.firstMenu`)}</span
+      >
+    {/if}
   </div>
 </button>
