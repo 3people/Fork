429224f92560087a750e694fbe69f65dab882d9f
3people <smsn7031@naver.com>
2024-09-27 18:45:01 +0900

    feat: add back button

--- src/components/FHeader.svelte
+++ src/components/FHeader.svelte
@@ -1,17 +1,36 @@
 <script>
   import ForkLogo from '../assets/icons/ForkLogo.svelte'
   import FDropdown from './FDropdown.svelte'
-  import {location} from 'svelte-spa-router'
+  import {location, pop} from 'svelte-spa-router'
   import {_} from 'svelte-i18n'
+  import {clsx} from 'clsx'
+  import ChevronLeft from '../assets/icons/ChevronLeft.svelte'
 
   $: displayDropdown = !['/', '/restaurant'].includes($location)
+  $: displayBackButton = !['/home', '/landing'].includes($location)
+
+  const onClickBackButton = () => {
+    pop()
+  }
 </script>
 
-<header class="px-4 py-2 flex justify-between w-[22.5rem] h-12 fixed top-0 z-10 bg-white">
-  <div class="flex gap-0.5 items-center">
-    <ForkLogo />
-    <span class="font-santokki text-brand-point leading-6">{$_('header.title')}</span>
-  </div>
+<header
+  {...$$restProps}
+  class={clsx(
+    'px-4 flex items-center justify-between w-full h-12 fixed top-0 z-10 bg-white',
+    $$restProps.class,
+  )}
+>
+  {#if displayBackButton}
+    <button class="cursor-pointer" on:click={onClickBackButton}>
+      <ChevronLeft />
+    </button>
+  {:else}
+    <div class="flex gap-0.5 items-center">
+      <ForkLogo />
+      <span class="font-santokki text-brand-point leading-6">{$_('header.title')}</span>
+    </div>
+  {/if}
   {#if displayDropdown}
     <FDropdown />
   {/if}
