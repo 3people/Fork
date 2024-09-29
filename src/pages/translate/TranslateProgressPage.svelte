f2162c550212ca7b7254cdee2b0ef15db8caea5e
3people <smsn7031@naver.com>
2024-09-30 01:37:29 +0900

    design: fix

--- src/pages/translate/TranslateProgressPage.svelte
+++ src/pages/translate/TranslateProgressPage.svelte
@@ -12,9 +12,7 @@
 
 <div class="w-full h-full mt-12 flex justify-center absolute top-0 left-0 bg-white px-5">
   <div class="w-full flex flex-col">
-    <div class="h-[33rem] flex items-center justify-center object-contain">
-      <img src={$imageStore.src} alt="menu" />
-    </div>
-    <FButton on:click={fetchImage}>확인</FButton>
+    <img class="w-full h-[33rem] object-contain" src={$imageStore.src} alt="menu" />
+    <FButton class="mt-4" on:click={fetchImage}>확인</FButton>
   </div>
 </div>
